09414d8e2f5ca85933f200c074c3091421a241d6
Betty Mutai <bettymutai00@gmail.com>
2017-07-07 17:50:38 +0300

    Created a get and post method for my definition

--- app.rb
+++ app.rb
@@ -2,18 +2,19 @@ require('sinatra')
   require('sinatra/reloader')
   also_reload('lib/**/*.rb')
   require('./lib/word')
+  require('./lib/definition')
 
   get('/') do
     erb(:index)
   end
 
+  get('/words/new') do
+    erb(:words_form)
+  end
+
   get('/words') do
     @words = Word.all()
     erb(:words)
-  end
-
-  get('/words/new') do
-    erb(:words_form)
   end
 
   post('/words') do
@@ -23,7 +24,26 @@ require('sinatra')
     erb(:success)
   end
 
+  get('/definitions/:id') do
+    @definition = Vehicle.find(params.fetch('id').to_i())
+    erb(:definition)
+  end
+
   get('/words/:id') do
-    @word = Word.find(params.fetch("id"))
+    @word = Word.find(params.fetch("id").to_i())
     erb(:word)
   end
+
+  get('/words/:id/definitions/new') do
+      @word = Word.find(params.fetch('id').to_i())
+      erb(:definitions_form)
+  end
+
+  post('/definitions') do
+   definition = params.fetch('definition')
+   @definition = Definition.new(definition)
+   @definition.save()
+   @word = Word.find(params.fetch('word_id').to_i())
+   @word.add_definition(@definition)
+   erb(:success)
+ end
