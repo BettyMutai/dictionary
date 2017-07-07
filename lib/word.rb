3985b26d7a71c7c0540e95a4d28515a0bd974f72
Betty Mutai <bettymutai00@gmail.com>
2017-07-07 12:58:05 +0300

    Added logic for my class word

---
+++ lib/word.rb
@@ -0,0 +1,24 @@
+class Word
+  @@all_words=[]
+
+  define_method(:initialize) do |word|
+    @word = word
+  end
+
+  define_method(:word) do
+    @word
+  end
+
+  define_singleton_method(:all) do
+    @@all_words
+  end
+
+  define_method(:save) do
+    @@all_words.push(self)
+  end
+
+  define_singleton_method(:clear) do
+     @@all_words = []
+   end
+
+end
