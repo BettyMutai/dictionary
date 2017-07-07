fefc06c4514116d666c25e85c163c2e8cfea6d87
Betty Mutai <bettymutai00@gmail.com>
2017-07-07 15:23:25 +0300

    Added spec for finding words and identification of words

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -35,4 +35,14 @@ require('rspec')
     end
   end
 
+  # describe(".find") do
+  #   it("returns a word by its id number") do
+  #     test_word = Word.new("Cow")
+  #     test_word.save()
+  #     test_word2 = Word.new("Apple")
+  #     test_word2.save()
+  #     expect(Word.find(test_word.id())).to(eq(test_word))
+  #   end
+  # end
+
   end
